f87f1238c9a5f1ecea619eb1c59b6cdba3a66f52
LucaScanu <lucagscanu@gmail.com>
2017-02-20 23:23:17 +0000

    added register controller and registration html form

---
+++ src/js/controllers/register.controller.js
@@ -0,0 +1,16 @@
+angular
+.module('secretChef')
+.controller('RegisterCtrl', RegisterCtrl);
+
+RegisterCtrl.$inject = ['User'];
+function RegisterCtrl(User) {
+  const vm = this;
+  vm.register = () => {
+    User.register(vm.user).$promise
+    .then(data => {
+      console.log(data);
+    },err => {
+      console.log(err);
+    });
+  };
+}
